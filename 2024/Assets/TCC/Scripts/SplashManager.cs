1802453956323bd0bc9e587e000bfa15c85c4bf8
PRISCILA RAFAELA LIMA DANTAS <d.priscila@academico.ifrn.edu.br>
2024-04-18 09:31:43 -0300

    interface

--- 2024/Assets/TCC/Scripts/SplashManager.cs
+++ 2024/Assets/TCC/Scripts/SplashManager.cs
@@ -7,7 +7,7 @@ public class SplashManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("LoadMainMenu", 10f);
+        Invoke("LoadMainMenu", 3f);
     }
 
     private void LoadMainMenu()
